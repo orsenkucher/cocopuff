8fc54312a336c0a6e741a249dccd9e6c06220075
Arseniy Kucher <orsen.kucher@gmail.com>
2021-03-18 01:15:53 +0200

    graphql gateway-service

---
+++ graphql/graph/resolver.go
@@ -0,0 +1,12 @@
+//go:generate gqlgen
+package graph
+
+import "github.com/orsenkucher/graphql/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
