654d7a850d7ffdcaef792c3e05deb6d90bb092e1
Arseniy Kucher <orsen.kucher@gmail.com>
2021-04-08 04:58:37 +0300

    feat: zap

--- account/cmd/main.go
+++ account/cmd/main.go
@@ -5,28 +5,44 @@ import (
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/orsenkucher/cocopuff/account"
+	"go.uber.org/zap"
 )
 
 const service = "account"
 
 type Specification struct {
 	Port int
+	Dev  bool
 	DSN  string
 }
 
 func main() {
-	if err := run(); err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func run() error {
 	var spec Specification
 	err := envconfig.Process(service, &spec)
 	if err != nil {
-		return err
+		log.Fatalln(err)
 	}
 
+	var logger *zap.Logger
+	if spec.Dev {
+		logger, err = zap.NewDevelopment()
+	} else {
+		logger, err = zap.NewProduction()
+	}
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer func() { _ = logger.Sync() }()
+	sugar := logger.Sugar()
+
+	if err := run(spec); err != nil {
+		sugar.Fatal(err)
+	}
+}
+
+func run(spec Specification) error {
 	repo, err := account.NewPostgresRepository(spec.DSN)
 	if err != nil {
 		return err
