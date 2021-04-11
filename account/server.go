a362d8cdd91a4002ac0259766a0f310ef55c23c6
Arseniy Kucher <orsen.kucher@gmail.com>
2021-04-11 05:35:17 +0300

    feat: GracefulStop, mark some code as pub

--- account/server.go
+++ account/server.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"sync"
 
 	"github.com/orsenkucher/cocopuff/account/pb"
 	"google.golang.org/grpc"
@@ -30,8 +31,33 @@ func ListenGRPC(ctx context.Context, service AccountService, port int) error {
 
 	pb.RegisterAccountServiceServer(srv, &AccountServiceServer{service: service})
 
-	return srv.Serve(lis)
-	// TODO: srv.GracefulStop()
+	select {
+	case err := <-ec(func() error { return srv.Serve(lis) }):
+		return err
+	case <-ctx.Done():
+		srv.GracefulStop()
+		return ctx.Err()
+	}
+}
+
+// pub: ec, wg
+func ec(f func() error) <-chan error {
+	ch := make(chan error)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		ch <- f()
+	}()
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	return ch
 }
 
 func (s *AccountServiceServer) CreateAccount(ctx context.Context, req *pb.CreateAccountRequest) (*pb.Account, error) {
