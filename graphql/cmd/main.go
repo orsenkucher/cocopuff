44f4476ff3a45f872a20fa3438a590a3c158a49e
Arseniy Kucher <orsen.kucher@gmail.com>
2021-04-12 05:51:51 +0300

    feat(graphql): cmd main

--- graphql/cmd/main.go
+++ graphql/cmd/main.go
@@ -1,38 +1,86 @@
 package main
 
 import (
-	"log"
+	"context"
 	"net/http"
 	"strconv"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/kelseyhightower/envconfig"
+	"github.com/orsenkucher/cocopuff/graphql"
+	"github.com/orsenkucher/cocopuff/graphql/dataloader"
+	"github.com/orsenkucher/cocopuff/graphql/env"
 	"github.com/orsenkucher/cocopuff/graphql/gql"
+	"github.com/orsenkucher/cocopuff/graphql/log"
 	"github.com/orsenkucher/cocopuff/graphql/resolver"
+	_ "go.uber.org/automaxprocs"
+	"go.uber.org/zap"
 )
 
 const service = "graphql"
 
-type Specification struct {
+type specification struct {
 	Port       int    `default:"9100"`
 	AccountURL string `envconfig:"ACCOUNT_SERVICE_URL"`
+
+	Release    bool
+	Version    string `default:"v0.0.0"`
+	Deployment string `default:"local"`
 }
 
 func main() {
-	var spec Specification
-
+	var spec specification
 	err := envconfig.Process(service, &spec)
 	if err != nil {
-		log.Fatalln(err)
+		log.Abortf("can't populate specification: %v", zap.Error(err))
 	}
 
-	srv := handler.NewDefaultServer(gql.NewExecutableSchema(gql.Config{Resolvers: &resolver.Resolver{}}))
+	sugar, err := log.New(service, spec.Deployment, spec.Version, spec.Release)
+	if err != nil {
+		log.Abortf("can't initialize zap logger: %v", zap.Error(err))
+	}
+
+	defer func() { _ = sugar.Sync() }()
+
+	defer func() {
+		if r := recover(); r != nil {
+			sugar.Error("recovered error", zap.Any("description", r))
+		}
+	}()
+
+	ctx := ctx(spec)
+	_ = ctx // TODO
+
+	// TODO: move to run()
+	// how about error logging in run?
+	client, err := graphql.NewClient(sugar, spec.AccountURL)
+	if err != nil {
+		sugar.Fatal("fail to dial:", zap.Error(err))
+	}
+
+	defer client.Close()
+
+	srv := handler.NewDefaultServer(gql.NewExecutableSchema(gql.Config{
+		Resolvers: resolver.NewResolver(sugar, client),
+	}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", dataloader.Middleware(srv, client))
 
 	port := strconv.Itoa(spec.Port)
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	sugar.Infof("connect to http://localhost:%s/ for GraphQL playground", port)
+	sugar.Fatal(http.ListenAndServe(":"+port, nil))
+	// TODO: http.GracefulStop
+	// TODO: move to server.go
+}
+
+func ctx(spec specification) context.Context {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, env.Service, service)
+	ctx = context.WithValue(ctx, env.Version, spec.Version)
+	ctx = context.WithValue(ctx, env.Release, spec.Release)
+	ctx = context.WithValue(ctx, env.Deployment, spec.Deployment)
+	ctx = context.WithValue(ctx, env.Tags, []string{spec.Deployment, spec.Version})
+	return ctx
 }
--- graphql/resolver/resolver.go
+++ graphql/resolver/resolver.go
@@ -1,7 +1,19 @@
 package resolver
 
+import (
+	"github.com/orsenkucher/cocopuff/graphql"
+	"go.uber.org/zap"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your service, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	sugar  *zap.SugaredLogger
+	client *graphql.Client
+}
+
+func NewResolver(sugar *zap.SugaredLogger, client *graphql.Client) *Resolver {
+	return &Resolver{sugar: sugar, client: client}
+}
