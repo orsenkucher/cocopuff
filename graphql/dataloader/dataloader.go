28c9b81e639f374186929f2e30b5f032c0129d67
Arseniy Kucher <orsen.kucher@gmail.com>
2021-04-12 05:50:43 +0300

    feat(graphql): dataloader middleware

--- graphql/dataloader/dataloader.go
+++ graphql/dataloader/dataloader.go
@@ -1,3 +1,47 @@
 //go:generate dataloaden AccountLoader string *github.com/orsenkucher/cocopuff/graphql.Account
 
 package dataloader
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/orsenkucher/cocopuff/graphql"
+	"github.com/orsenkucher/cocopuff/graphql/env"
+)
+
+type Dataloader struct {
+	AccountById *AccountLoader
+}
+
+func Middleware(next http.Handler, client *graphql.Client) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), env.Dataloader, &Dataloader{
+			AccountById: NewAccountLoader(NewAccountLoaderConfig(r.Context(), client)),
+		})
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func For(ctx context.Context) *Dataloader {
+	return ctx.Value(env.Dataloader).(*Dataloader)
+}
+
+func NewAccountLoaderConfig(ctx context.Context, client *graphql.Client) AccountLoaderConfig {
+	return AccountLoaderConfig{
+		MaxBatch: 100,
+		Wait:     1 * time.Millisecond,
+		Fetch: func(ids []string) ([]*graphql.Account, []error) {
+			// TODO: GetAccounts()
+			res, err := client.ListAccounts(ctx, 0, 0)
+			if err != nil {
+				// single error for everything
+				return nil, []error{err}
+			}
+
+			return res, nil
+		},
+	}
+}
