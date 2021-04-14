ef82df13f3847b25909ab0d7106dfd5035177c94
Arseniy Kucher <orsen.kucher@gmail.com>
2021-04-15 00:29:26 +0300

    feat(pub): package care

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/matryer/moq v0.2.1 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/segmentio/ksuid v1.0.3
 	github.com/urfave/cli/v2 v2.3.0 // indirect
---
+++ pub/care/care.go
@@ -0,0 +1,120 @@
+package care
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type zapcare struct {
+	source  error
+	message string
+	fields  []zap.Field
+}
+
+func New(message string, fields ...zap.Field) error {
+	return zapcare{
+		source: errors.New(message),
+		fields: fields,
+	}
+}
+
+func Of(err error, message string, fields ...zap.Field) error {
+	if err == nil {
+		return nil
+	}
+
+	if err, ok := err.(zapcare); ok {
+		if len(fields) == 0 {
+			err.source = errors.Wrap(err.source, message)
+			return err
+		}
+
+		if len(err.fields) == 0 {
+			return withFields(errors.Wrap(err.source, message), fields...)
+		}
+
+		return zapcare{
+			source:  err,
+			fields:  fields,
+			message: message,
+		}
+	}
+
+	return withFields(errors.Wrap(err, message), fields...)
+}
+
+func (e zapcare) Error() string {
+	if e.message != "" {
+		return e.message + ": " + e.source.Error()
+	}
+
+	return e.source.Error()
+}
+
+func (e zapcare) Unwrap() error {
+	return e.source
+}
+
+func withFields(err error, fields ...zap.Field) error {
+	if err == nil {
+		return nil
+	}
+
+	if err, ok := err.(zapcare); ok {
+		err.fields = append(err.fields, fields...)
+		return err
+	}
+
+	return zapcare{
+		source: err,
+		fields: fields,
+	}
+}
+
+func ToZap(err error) zap.Field {
+	return ToNamedZap("error", err)
+}
+
+func ToNamedZap(key string, err error) zap.Field {
+	if err == nil {
+		return zap.Skip()
+	}
+
+	if e, ok := err.(zapcore.ObjectMarshaler); ok {
+		return zap.Object(key, e)
+	}
+
+	return zap.NamedError(key, err)
+}
+
+func (e zapcare) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	for _, f := range e.fields {
+		f.AddTo(enc)
+	}
+
+	if e.message != "" {
+		zap.String("message", e.message).AddTo(enc)
+	}
+
+	ToZap(e.source).AddTo(enc)
+	return nil
+}
+
+func (e zapcare) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 'v':
+		if s.Flag('+') {
+			fmt.Fprintf(s, "%+v\n", e.source)
+			if e.message != "" {
+				fmt.Fprint(s, e.message)
+			}
+			return
+		}
+		fallthrough
+	case 's', 'q':
+		fmt.Fprint(s, e.Error())
+	}
+}
