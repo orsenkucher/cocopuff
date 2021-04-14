44cc9a057b1056bcc0bb057204b6a0ff64c07a39
Arseniy Kucher <orsen.kucher@gmail.com>
2021-04-14 19:35:52 +0300

    ref(account): refactor context usage

--- account/cmd/main.go
+++ account/cmd/main.go
@@ -55,11 +55,7 @@ func main() {
 
 func ctx(spec specification) context.Context {
 	ctx := gs.With(context.Background())
-	ctx = context.WithValue(ctx, env.Service, service)
-	ctx = context.WithValue(ctx, env.Version, spec.Version)
-	ctx = context.WithValue(ctx, env.Release, spec.Release)
-	ctx = context.WithValue(ctx, env.Deployment, spec.Deployment)
-	ctx = context.WithValue(ctx, env.Tags, []string{spec.Deployment, spec.Version})
+	ctx = env.With(ctx, service, spec.Deployment, spec.Version, spec.Release)
 	return ctx
 }
 
