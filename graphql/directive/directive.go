6062db4084fe69620c0dc8199e1be9ea24130007
Arseniy Kucher <orsen.kucher@gmail.com>
2021-04-16 05:15:29 +0300

    ref: use `super` alias to refer to the parent scope

--- graphql/directive/directive.go
+++ graphql/directive/directive.go
@@ -3,16 +3,16 @@ package directive
 import (
 	"context"
 
-	graph "github.com/99designs/gqlgen/graphql"
-	"github.com/orsenkucher/cocopuff/graphql"
+	"github.com/99designs/gqlgen/graphql"
+	super "github.com/orsenkucher/cocopuff/graphql"
 	"github.com/orsenkucher/cocopuff/graphql/gql"
 	"github.com/orsenkucher/cocopuff/pub/care"
 	"go.uber.org/zap"
 )
 
-func New(sugar *zap.SugaredLogger, client *graphql.Client) gql.DirectiveRoot {
+func New(sugar *zap.SugaredLogger, client *super.Client) gql.DirectiveRoot {
 	return gql.DirectiveRoot{
-		HasRole: func(ctx context.Context, obj interface{}, next graph.Resolver, role gql.Role) (interface{}, error) {
+		HasRole: func(ctx context.Context, obj interface{}, next graphql.Resolver, role gql.Role) (interface{}, error) {
 			w := care.With(zap.String("package", "directive"), zap.String("directive", "HasRole"))
 			sugar.Desugar().Info("", w.Fields...)
 
